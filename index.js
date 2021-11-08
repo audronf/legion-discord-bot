ffed57c3439eba204bf4875f473b341bd0891b18
Audronf <federicofasitella@gmail.com>
2021-11-08 16:43:33 -0300

    Lloriquin

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const { Client, Intents } = require('discord.js')
+const { Client, Intents } = require('discord.js');
 const config = require("./config.json");
+const state = require("./state.json");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
+const prefix = '!';
+
 client.on("message", function(message) {
     if (message.author.bot) return;
-    if (message.content.includes("lloriquin") && message.user != client) {
-        const user = message.content.split(' ')[1]
 
-        if (user) {
-            message.reply(user + " tiene el lloriquín")
-        } else {
-            message.reply("Tenés que decirme quien tiene el lloriquin, ahora lo tenes vos.")
+    if (message.content.startsWith(prefix + "lloriquin ") && message.user != client) {
+        try {
+            const user = message.mentions.users.first().username ? message.mentions.users.first().username : false
+            if (user) {
+                let u = state.users.find(u => {
+                    return u.name == user });
+                if (u) {
+                    message.reply(user + " tiene el lloriquín. Ya se lo dieron " + u.times + " veces.")
+                }
+            } else {
+                message.reply("Tenés que decirme quien tiene el lloriquin, ahora lo tenes vos.")
+            }
+        } catch(error) {
+            message.reply("Poné bien el comando plis");
         }
     }
 });
